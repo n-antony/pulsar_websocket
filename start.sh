afa84f74aa452d5fbb83f9c837fa163f190c5996
n-antony <31750184+n-antony@users.noreply.github.com>
2025-03-10 16:25:15 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,15 +7,15 @@ export PATH=$JAVA_HOME/bin:$PATH
 # Check if Java is already installed
 if [ ! -d "$JAVA_HOME" ]; then
   echo "Downloading and extracting OpenJDK 17..."
-  curl -LO https://download.java.net/java/GA/jdk17/0/GPL/openjdk-17_linux-x64_bin.tar.gz
-  tar -xzf openjdk-17_linux-x64_bin.tar.gz
-  mv jdk-17.* jdk-17
+  curl -LO https://download.oracle.com/java/17/archive/jdk-17.0.12_linux-x64_bin.tar.gz
+  tar -xzf jdk-17.0.12_linux-x64_bin.tar.gz
+  mv jdk-17.0.12 jdk-17
 fi
 
 # Download & extract Pulsar if not already present
 if [ ! -d "apache-pulsar-4.0.3" ]; then
   echo "Downloading and extracting Pulsar..."
-  curl -LO "https://www.apache.org/dyn/closer.lua/pulsar/pulsar-4.0.3/apache-pulsar-4.0.3-bin.tar.gz?action=download"
+  curl -LO "https://downloads.apache.org/pulsar/pulsar-4.0.3/apache-pulsar-4.0.3-bin.tar.gz"
   tar xvfz apache-pulsar-4.0.3-bin.tar.gz
 fi
 
